7b93e6ad068a99ff872ef84cd1db2d5fc0a8551e
Answermouth <ansel.gamet@telecomnancy.net>
2018-12-03 19:09:59 +0100

    Add edge weights

--- visu/myscript.js
+++ visu/myscript.js
@@ -1,3 +1,5 @@
+var nodeClicked = false;
+
 $(document).ready(function() {
     const url = "http://localhost:7474/";
     const username = "neo4j";
@@ -22,7 +24,7 @@ $(document).ready(function() {
         error: function(error) {
             console.log(`Error ${error}`);
         }  
-    })
+    })    
 })
 
 function convertResults(result) {
@@ -44,7 +46,7 @@ function convertResults(result) {
     });
     viz = {nodes:nodes, links:links};
     console.log(viz);
-    createGraph(viz);
+    createGraph(viz);    
 }
 
 function createGraph(json) {
@@ -53,21 +55,19 @@ function createGraph(json) {
     var svg = d3.select("#graph")
             .append("svg")
             .attr("width", "100%")
-            .attr("height", "100%")
+            .attr("height", "100%")                      
             .call(d3.behavior.zoom().on("zoom", function () {
-                svg.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")")
-            }))
-            .append("g");
+                if (nodeClicked == false)
+                    svg.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")")
+            }))            
+            .append("g");        
 
     var force = d3.layout.force()
         .gravity(0.05)
         .distance(100)
         .charge(-100)
         .size([width, height]);
-
-    console.log(force);
-    console.log(json.nodes);
-    console.log(json.links);
+    
     force.nodes(json.nodes)
         .links(json.links)
         .start();
@@ -84,13 +84,52 @@ function createGraph(json) {
         .call(force.drag);
 
     node.append("circle")
-            .attr("class", function (d) { return "node "+d.label })
-            .attr("r", 10);
+            .attr("class", function (d) { return "node circle" })
+            .attr("r", 10)
+            .on("mousedown", function() {                
+                nodeClicked = true;
+            })
+            .on("mouseup", function() {
+                nodeClicked = false;
+            });
 
     node.append("text")
         .attr("dx", 12)
         .attr("dy", ".35em")
         .text(function(d) { return d.title });
+
+
+    var edgepaths = svg.selectAll(".edgepath")
+        .data(json.links)
+        .enter()
+        .append('path')
+        .attr({'d': function(d) {return 'M '+d.source.x+' '+d.source.y+' L '+ d.target.x +' '+d.target.y},
+            'class':'edgepath',
+            'fill-opacity':0,
+            'stroke-opacity':0,
+            'fill':'blue',
+            'stroke':'red',
+            'id':function(d,i) {return 'edgepath'+i}})
+        .style("pointer-events", "none");
+
+    var edgelabels = svg.selectAll(".edgelabel")
+        .data(json.links)
+        .enter()
+        .append('text')
+        .style("pointer-events", "none")
+        .attr({'class':'edgelabel',
+               'id':function(d,i){return 'edgelabel'+i},
+               'dx':80,
+               'dy':0,
+               'font-size':10,
+               'fill':'#aaa'});
+
+    edgelabels.append('textPath')
+        .attr('xlink:href',function(d,i) {return '#edgepath'+i})
+        .style("pointer-events", "none")
+        .text(function(d,i){            
+            return d.weight.toFixed(2);
+        });
 
     force.on("tick", function() {
         link.attr("x1", function(d) { return d.source.x; })
@@ -99,6 +138,20 @@ function createGraph(json) {
             .attr("y2", function(d) { return d.target.y; });
 
         node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-    
+        
+        edgepaths.attr('d', function(d) { var path='M '+d.source.x+' '+d.source.y+' L '+ d.target.x +' '+d.target.y;                                           
+                                           return path});       
+
+        edgelabels.attr('transform',function(d,i){
+            if (d.target.x<d.source.x){
+                bbox = this.getBBox();
+                rx = bbox.x+bbox.width/2;
+                ry = bbox.y+bbox.height/2;
+                return 'rotate(180 '+rx+' '+ry+')';
+                }
+            else {
+                return 'rotate(0)';
+                }
+        });
     });
 }
